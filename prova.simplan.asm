4ab24213d9df8e85973fa110697d91d435bb1eb4
Sonia0912 <41631672+Sonia0912@users.noreply.github.com>
2023-05-31 23:39:11 +0200

    aggiornati altri file

--- prova.simplan.asm
+++ prova.simplan.asm
@@ -2,22 +2,16 @@ move SP FP
 pushr FP 
 move SP AL 
 pushr AL 
-storei A0 3
-pushr A0 
-storei A0 7
-pushr A0 
-storei A0 99
-pushr A0 
-move AL T1 
+nullmove AL T1 
 subi T1 1
 store A0 0(T1) 
 pushr A0 
 storei A0 1
 popr T1 
-add A0 T1 
-popr A0 
-pushr A0 
-move AL T1 
-subi T1 4
-store A0 0(T1) 
+beq A0 T1 label0
+storei A0 0
+b label1
+label0:
+storei A0 1
+label1:
 halt
